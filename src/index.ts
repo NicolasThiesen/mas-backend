6936434cc75dce6e6d93f12df1cd42b1242aaa56
Nícolas Medeiros Thiesen <49465248+NicolasThiesen@users.noreply.github.com>
2021-04-25 21:43:04 -0300

    New Files

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express =  require("express");
 import cors = require("cors");
+import routes from "./routes";
+import "./database";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(routes);
 
 app.listen(3333);
---
+++ src/routes.ts
@@ -0,0 +1,29 @@
+import {response, Router} from "express";
+
+const routes = Router();
+
+routes.get('/user/', (request, response) => {
+    const {nome, idade} = request.query
+        response.json({
+            idade,
+            nome
+            })
+    })
+routes.get("/user/:id", (request, response) => {
+            const {id} = request.params;
+            response.json({
+                userId:id
+            })
+});
+
+routes.post('/user', (request, response) => {
+    const {name, email, password} = request.body
+    const user = {
+    name,
+    email,
+    password
+    }
+    return response.json(user);
+})
+
+export default routes;
